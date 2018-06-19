afa7913bf32be69bc41b3deb9d56644234c31cc2
David <david.weining@outlook.com>
2018-06-19 15:43:28 +0800

    利用router将api模块化

---
+++ api/api.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+//3. 创建中间件:use
+//截取请求, 拦截回调
+router.use('/', function (request, response, next) {
+    // console.log('执行中间件')
+    // console.log('获取数据库数据')
+    // console.log(request.query.page)
+    next()
+})
+
+
+//3. 访问服务器(get或者post)
+//参数一: 请求根路径
+//3.1 get请求
+router.get('/getShops', function (request, response) {
+    // console.log(request)
+    response.send('get请求成功')
+})
+
+//3.2 post请求
+router.post('/bb', function (request, response) {
+    response.send('post请求成功')
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -18,28 +18,9 @@ require("./db/db.js");
 // app.use(express.static('dist'));
 app.use(express.static('static'));
 
-//3. 创建中间件:use
-//截取请求, 拦截回调
-app.use('/', function (request, response, next) {
-    // console.log('执行中间件')
-    // console.log('获取数据库数据')
-    // console.log(request.query.page)
-    next()
-})
+var apis = require('./api/api.js');
 
-
-//3. 访问服务器(get或者post)
-//参数一: 请求根路径
-//3.1 get请求
-app.get('/getShops', function (request, response) {
-    // console.log(request)
-    response.send('get请求成功')
-})
-
-//3.2 post请求
-app.post('/bb', function (request, response) {
-    response.send('post请求成功')
-})
+app.use('/', apis);
 
 //4. 绑定端口
 app.listen(8989)
